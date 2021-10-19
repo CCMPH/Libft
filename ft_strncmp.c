2517cf89b7d5082c08a40102b5f38ff96e8aa0db
Celeke Harteman <chartema@f0r6s10.codam.nl>
2021-10-19 11:20:34 +0200

    strncmp aangepast

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -2,7 +2,7 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while ((*s1 != '\0' || *s2 != '\0') && n > 0)
+	while (*s1 != '\0' && n > 0)
 	{
 		if (*s1 != *s2)
 			return ((unsigned char ) *s1 - (unsigned char ) *s2);
@@ -10,5 +10,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		s2++;
 		n--;
 	}
-	return ((unsigned char) *s1 - (unsigned char) *s2);
+	if (n > 0 && *s1 != *s2)
+		return ((unsigned char) *s1 - (unsigned char) *s2);
+	return (0);
 }
